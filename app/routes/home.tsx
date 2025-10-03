375b059fc51e41b8810446fa7c06872ea606cbd6
Akamshu <39032612+Akamshu@users.noreply.github.com>
2025-10-03 04:45:00 +0100

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -302,7 +302,7 @@ description:
       <AnimatePresence>
         {menuOpen && (
           <motion.div
-            className="fixed inset-0 z-50 bg-dark bg-opacity-80"
+            className="fixed inset-0 z-50 bg-white"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
