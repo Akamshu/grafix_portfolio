5b2fc70620b991653234993a1347a7971025c273
Akamshu <39032612+Akamshu@users.noreply.github.com>
2025-10-03 05:21:18 +0100

    Update home.tsx

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -233,6 +233,11 @@ description:
       gallery: [
         "/assets/img/projects/webui/Screenshot_20250929-133704_WhatsApp.jpg",
         "/assets/img/projects/webui/Screenshot_20250929-133708_WhatsApp.jpg",
+        "/assets/img/projects/webui/Screenshot_20250929-133708_WhatsApp.jpg",
+        "/assets/img/projects/webui/INDEP.jpg",
+        "/assets/img/projects/webui/IMG-20250715-WA0004(1).jpg",
+        "/assets/img/projects/webui/IMG-20251003-WA0000.jpg",
+        "/assets/img/projects/webui/IMG-20250924-WA0006.jpg",
         
       ],
     },
